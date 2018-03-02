3f20998e54e1d7c564d75abb59460393ef76170a
Joseph Pulaski <jpulaski79@gmail.com>
2018-03-02 08:09:57 +0200

    create Router component and basic Routing logic

---
+++ src/components/NotFound.js
@@ -0,0 +1,12 @@
+import React, {Fragment} from 'react';
+
+const NotFound = props => (<Fragment>
+	<div className="biz-review-app">
+		<div className="menu">
+			<h1>THE PAGE YOUR LOOKING FOR IS NOT FOUND</h1>
+		</div>
+
+	</div>
+</Fragment>);
+
+export default NotFound;
