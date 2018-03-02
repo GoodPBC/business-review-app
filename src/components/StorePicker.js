6774ef714032d307b6c2d14e046f7ae79ad0d9dd
Joseph Pulaski <jpulaski79@gmail.com>
2018-03-02 12:10:37 +0200

    bind this with constructor function

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,16 +1,40 @@
-import React, {Fragment} from 'react';
-import {getFunName} from '../helpers';
+import React, { Fragment } from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
-	render() {
-		return (<Fragment>
-			<form action="" className="store-selector">
-				<h2>PLEASE ENTER A BUSINESS</h2>
-				<input type="text" required="required" placeholder="Business Name" defaultValue={getFunName()}/>
-				<button type="submit">Visit Store</button>
-			</form>
-		</Fragment>);
-	}
+  constructor() {
+    super();
+    this.goToBusiness = this.goToBusiness.bind(this);
+  }
+  //empty ref
+  inputRef = React.createRef();
+
+  goToBusiness(event) {
+    //prevent page reload
+    event.preventDefault();
+    console.log('going the the store');
+    //grab text from input
+    console.log(this);
+    //go to business profile entered
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <form action="" className="store-selector" onSubmit={this.goToBusiness}>
+          <h2>PLEASE ENTER A BUSINESS</h2>
+          <input
+            type="text"
+            ref={this.inputRef}
+            required="required"
+            placeholder="Business Name"
+            defaultValue={getFunName()}
+          />
+          <button type="submit">Visit Store</button>
+        </form>
+      </Fragment>
+    );
+  }
 }
 
 export default StorePicker;
