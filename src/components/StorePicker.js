fe59cb91000954781593c481c3cb0f7b6f13a72a
Joseph Pulaski <jpulaski79@gmail.com>
2018-03-02 08:40:35 +0200

    import random Names helper function to StorePicker component

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,12 @@
 import React, {Fragment} from 'react';
+import {getFunName} from '../helpers';
 
 class StorePicker extends React.Component {
 	render() {
 		return (<Fragment>
 			<form action="" className="store-selector">
 				<h2>PLEASE ENTER A BUSINESS</h2>
-				<input type="text" required="required" placeholder="Business Name"/>
+				<input type="text" required="required" placeholder="Business Name" defaultValue={getFunName()}/>
 				<button type="submit">Visit Store</button>
 			</form>
 		</Fragment>);
